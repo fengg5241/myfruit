1af0fd0dd3014cc10977184d7d39b69a18df78b1
fengg5241 <fengg_5241@163.com>
2018-02-12 23:04:09 +0800

    add files

---
+++ config.php
@@ -0,0 +1,59 @@
+<?php
+// HTTP
+// define('HTTP_SERVER', 'http://myfruit.com.sg/');
+define('HTTP_SERVER', './');
+// HTTPS
+// define('HTTPS_SERVER', 'http://myfruit.com.sg/');
+define('HTTPS_SERVER', './');
+// DIR
+// define('DIR_APPLICATION', '/home/myfruitc/public_html/catalog/');
+// define('DIR_SYSTEM', '/home/myfruitc/public_html/system/');
+// define('DIR_LANGUAGE', '/home/myfruitc/public_html/catalog/language/');
+// define('DIR_TEMPLATE', '/home/myfruitc/public_html/catalog/view/theme/');
+// define('DIR_CONFIG', '/home/myfruitc/public_html/system/config/');
+// define('DIR_IMAGE', '/home/myfruitc/public_html/image/');
+// define('DIR_CACHE', '/home/myfruitc/public_html/system/storage/cache/');
+// define('DIR_DOWNLOAD', '/home/myfruitc/public_html/system/storage/download/');
+// define('DIR_LOGS', '/home/myfruitc/public_html/system/storage/logs/');
+// define('DIR_MODIFICATION', '/home/myfruitc/public_html/system/storage/modification/');
+// define('DIR_UPLOAD', '/home/myfruitc/public_html/system/storage/upload/');
+
+define('DIR_APPLICATION', 'C:/wamp64/apps/fruit/catalog/');
+define('DIR_SYSTEM', 'C:/wamp64/apps/fruit/system/');
+define('DIR_LANGUAGE', 'C:/wamp64/apps/fruit/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp64/apps/fruit/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp64/apps/fruit/system/config/');
+define('DIR_IMAGE', 'C:/wamp64/apps/fruit/image/');
+define('DIR_CACHE', 'C:/wamp64/apps/fruit/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp64/apps/fruit/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp64/apps/fruit/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp64/apps/fruit/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp64/apps/fruit/system/storage/upload/');
+
+
+
+
+// define('DIR_APPLICATION', 'C:/wamp64/apps/fruit/catalog/');
+// define('DIR_SYSTEM', 'C:/wamp64/apps/fruit/system/');
+// define('DIR_IMAGE', 'C:/wamp64/apps/fruit/image/');
+// define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+// define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+// define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+// define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+// define('DIR_CACHE', DIR_STORAGE . 'cache/');
+// define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+// define('DIR_LOGS', DIR_STORAGE . 'logs/');
+// define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+// define('DIR_SESSION', DIR_STORAGE . 'session/');
+// define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+// define('DB_USERNAME', 'myfruitc_online');
+// define('DB_PASSWORD', 'online$2016');
+define('DB_USERNAME', 'feng');
+define('DB_PASSWORD', 'feng');
+define('DB_DATABASE', 'myfruitc_online');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
